280fe3ff89cf428c7d1afd3a2791e928048f9572
anuskastha212 <116198700+anuskastha212@users.noreply.github.com>
2024-11-19 20:17:18 +0545

    update

---
+++ swap_num/swap_num/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Predefined numbers
+            int a = 10;
+            int b = 20;
+
+            Console.WriteLine($"Before swapping: a = {a}, b = {b}");
+
+            // Swap numbers without using a third variable
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine($"After swapping: a = {a}, b = {b}");
+        }
+    }
+}
